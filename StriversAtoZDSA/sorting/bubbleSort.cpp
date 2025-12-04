7dbef6f2f00e5405a730c3a83315393a8d92c9b1
Abhishekk-AK <abhishek520abc@gmail.com>
2025-12-04 13:42:08 +0530

    bubble-sort

---
+++ StriversAtoZDSA/sorting/bubbleSort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> bubbleSort(vector<int>& nums) {
+
+    for(int i=nums.size()-1; i>=1; i--) {
+        for(int j=0; j<=i-1; j++) {
+            if(nums[j]>nums[j+1]) {
+                int temp=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+        }
+    }
+
+    return nums;
+}
+
+int main() {
+    vector<int> v = {1,4,6,32,2,9,3,5,0,9,22,66};
+
+    vector<int>ans = bubbleSort(v);
+    for(auto x: ans) {
+        cout<<x<<" ";
+    }
+
+    return 0;
+}
