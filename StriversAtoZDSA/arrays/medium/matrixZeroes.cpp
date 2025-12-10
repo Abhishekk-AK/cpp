15d57f07f68eb7f38ba2864de42fe46b8e520d93
Abhishekk-AK <abhishek520abc@gmail.com>
2025-12-10 23:50:09 +0530

    add matrix-zeroes optimal solution

--- StriversAtoZDSA/arrays/medium/matrixZeroes.cpp
+++ StriversAtoZDSA/arrays/medium/matrixZeroes.cpp
@@ -63,11 +63,76 @@ void setZeros(vector<vector<int>>& matrix) {
         }
     }
 
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
             if(row[i]==1 || col[j]==1) {
                 matrix[i][j]=0;
             }
+        }
+    }
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int m=matrix.size();
+    int n=matrix[0].size();
+
+    bool firstRowZero=false;
+    bool firstColZero=false;
+
+    //check if first row has 0
+    for(int j=0; j<n; j++) {
+        if(matrix[0][j]==0) {
+            firstRowZero=true;
+            break;
+        }
+    }
+
+    //check if first col has 0
+    for(int i=0; i<m; i++) {
+        if(matrix[i][0]==0) {
+            firstColZero=true;
+            break;
+        }
+    }
+
+    //mark 0s
+    for(int i=1; i<m; i++) {
+        for(int j=1; j<n; j++) {
+            if(matrix[i][j]==0) {
+                matrix[i][0]=0;
+                matrix[0][j]=0;
+            }
+        }
+    }
+
+    //set 0s
+    for(int i=1; i<m; i++) {
+        for(int j=1; j<n; j++) {
+            if(matrix[i][0]==0 || matrix[0][j]==0) {
+                matrix[i][j]=0;
+            }
+        }
+    }
+
+    //handle first row
+    if(firstRowZero) {
+        for(int j=0; j<n; j++) {
+            matrix[0][j]=0;
+        }
+    }
+
+    //hanle first col
+    if(firstColZero) {
+        for(int i=0; i<m; i++) {
+            matrix[i][0]=0;
         }
     }
 
@@ -121,6 +186,9 @@ int main() {
 
     cout<<"set zeros is:"<<endl;
     setZeros(mt);
+
+    cout<<"set zeroes is:"<<endl;
+    setZeroes(mt);
 
     return 0;
 }
