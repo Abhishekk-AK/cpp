e7876d3fecbb9efb000790cfe02ee1f8736efc3b
Abhishekk-AK <abhishek520abc@gmail.com>
2025-12-19 23:19:42 +0530

    add search in 2d matrix sol

---
+++ StriversAtoZDSA/binary search/2d-Arrays/searchIn2dMatrix.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+bool searchMat(vector<vector<int>>& matrix, int target) {
+    int m=matrix.size();
+    int n=matrix[0].size();
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            if(matrix[i][j]==target) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int searchMatrix(vector<vector<int>>& mat, int target) {
+    int m=mat.size();
+    int n=mat[0].size();
+
+    for(int i=0; i<m; i++) {
+        int low=0; int high=n-1;
+
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+
+            if(mat[i][mid]==target) {
+                return true;
+            }
+            else if(mat[i][mid]<target) {
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+    }
+    return false;
+}
+
+
+int main() {
+
+    int m,n;
+    cout<<"rows: ";
+    cin>>m;
+    cout<<"columns: ";
+    cin>>n;
+
+    vector<vector<int>> mt(m, vector<int>(n));
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> mt[i][j];
+        } 
+    }
+
+    cout<<"Matrix is: "<<endl;
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            cout << mt[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    int k;
+    cin>>k;
+    cout<<endl;
+
+    cout<<searchMat(mt, k)<<endl;
+
+    cout<<searchMatrix(mt, k);
+
+    return 0;
+}
